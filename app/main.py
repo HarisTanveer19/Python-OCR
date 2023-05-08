3a766c67aacf5c93a00db1c5f7149e6dc95acff4
HarisTanveer19 <haristanveer19@gmail.com>
2023-05-09 01:00:37 +0500

    Finalized Code

--- app/main.py
+++ app/main.py
@@ -1,21 +1,31 @@
 from typing import Union
 from fastapi import FastAPI, File, UploadFile
+import pytesseract
 from PIL import Image
 import pathlib
 import uuid
 
 app = FastAPI()
 
-# BASE_DIR = pathlib.Path(__file__).parent
-IMG_DIR =  "images/"
+
+IMG_DIR = "images/"
+img_path = "/home/haris/Desktop/FastAPI_OCR/app/images/test2.png"
+text_path = "/home/haris/Desktop/FastAPI_OCR/app/Text"
 
 @app.post('/')
 async def Upload_img(file: UploadFile = File(...)):
-    # file.filename = f"{uuid.uuid1().jpg}"
-    file.filename = f"{uuid.uuid1()}.png"
-    file.filename = f"{uuid.uuid4()}.jpg"
+    # Reading FIle Content
     content = await file.read()
-
+    # Creating File in Image Directory
     with open(f"{IMG_DIR}{file.filename}", "wb") as f:
         f.write(content)
+    # Opening image and converting into text
+    img = Image.open(img_path)
+    result = pytesseract.image_to_string(img)
+    # Creating text file and saving
+    f = open("/home/haris/Desktop/FastAPI_OCR/app/Text/result.txt", "w")
+    f.write(result)
+    f.close
+
+
     return {"filename": file.filename} 
